e66c52f9d2fd9ac6d8088f729e6026421b313ddc
Abhay Shukla <79450769+Abhay217@users.noreply.github.com>
2021-06-10 23:54:33 +0530

    Add files via upload

---
+++ forgot/functions/function.php
@@ -0,0 +1,21 @@
+<?php
+
+include("../include/otp-write.php" );
+
+
+
+// if(isset($_POST['mobile_number'])){
+
+// include("../include/controller.php" );
+// echo "lkojoij;oijk";
+// }
+//     function_name();
+//     die();
+    
+// }
+
+// function function_name(){
+//     return 'hellow';
+// }
+
+?>
